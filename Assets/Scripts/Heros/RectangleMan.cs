c348e06ece07e5a67c7ce6bd1d178c3d21816a61
Michael Dibonaventura <dibonaventura69@gmail.com>
2024-03-28 18:30:20 -0400

    added animations and mesh to rectangle man

--- Assets/Scripts/Heros/RectangleMan.cs
+++ Assets/Scripts/Heros/RectangleMan.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class RectangleMan : HeroBase
 {
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            PlayerController.Player.baseAbility1 = new Ability(1f, 5f);
+            PlayerController.Player.baseAbility2 = new Ability(1f, 3f);
+            PlayerController.Player.baseAbility3 = new Ability(1f, 10f);
+            HeroBase player = PlayerController.Player;
+            HeroUI.Instance.SetUltSlider();
+            if (player == null)
+            {
+                Debug.LogError("Player is not set yet.");
+                return;
+            }
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+    }
 
 }

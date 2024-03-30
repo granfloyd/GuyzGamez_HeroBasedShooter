1be0c22164ef0a2113669cbc808b08e8336b9a7e
Michael Dibonaventura <dibonaventura69@gmail.com>
2024-03-29 20:36:40 -0400

    testing relay

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -1,49 +1,156 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Net;
+using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Services.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Relay.Models;
+using TMPro;
 
-public class NetworkUI : NetworkBehaviour
-{
-    public Button startServerButton;
-    public Button startClientButton;
-    public PlayerController pc;
-    // Start is called before the first frame update
-    void Start()
+public class NetworkUI : MonoBehaviour
+{ 
+    public GameObject hostClientUI;
+    public GameObject hostPickMapUI;//map select
+    public GameObject clientEnterCodeUI;//enter code to joni host
+
+    public Button serverButton;
+    public Button clientButton;
+    public Button backButton;
+
+    public Button mapButton;
+
+    public TMP_Text inputFieldText;
+    public Button inputFieldTextButton;
+
+    public bool isHost = false;
+    public bool isClient = false;
+    public string map1 = "TestingMap";
+
+    private async void Start()
     {
-       startServerButton.onClick.AddListener(Host);
-       startClientButton.onClick.AddListener(Client);
+        await UnityServices.InitializeAsync();//pasuses here until reply
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        serverButton.onClick.AddListener(SelectHost);
+        clientButton.onClick.AddListener(SelectClient);
+        backButton.onClick.AddListener(RemoveOrAdd);
+
+        mapButton.onClick.AddListener(JoinMap);//host now makes game 
+        inputFieldTextButton.onClick.AddListener(JoinGame);//trys to connect client to game
+
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
     }
-    void Host()
+    private async void CreateRelay()
     {
-        NetworkManager.Singleton.StartHost();
-        RemoveFromScreen();
-    }
-    void Client()
-    {
-        NetworkManager.Singleton.StartClient();
-        RemoveFromScreen();
+        try
+        {
+            //parm is how many clients not including host so max is 4 players
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            Debug.Log("Join code: " + joinCode);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+                allocation.RelayServer.IpV4,
+                (ushort)allocation.RelayServer.Port,
+                allocation.AllocationIdBytes,
+                allocation.Key,
+                allocation.ConnectionData);
+
+            NetworkManager.Singleton.StartHost();
+            NetworkManager.Singleton.SceneManager.LoadScene(map1, UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError(e);
+        }
+
     }
 
-    private void RemoveFromScreen()
+    private async void JoinRelay(string joinCode)
     {
-        startServerButton.gameObject.SetActive(false);
-        startClientButton.gameObject.SetActive(false);
+        try
+        {
+            joinCode = inputFieldText.text;
+            Debug.Log("Joining relay with code: " + joinCode);
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort)joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.ConnectionData,
+                joinAllocation.HostConnectionData);
+
+            NetworkManager.Singleton.StartClient();
+            NetworkManager.Singleton.SceneManager.LoadScene(map1, UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError(e);
+        }
     }
+    void SelectHost()
+    {
+        isHost = true;
+        RemoveOrAdd();
+    }
+    void SelectClient()
+    {
+        isClient = true;
+        RemoveOrAdd();
+    }
+    private void RemoveOrAdd()
+    {
+        if(!backButton.gameObject.activeSelf)
+        {
+            if (isHost)
+            {
+                hostClientUI.SetActive(false);
+                hostPickMapUI.SetActive(true);
+            }
+            else if (isClient)
+            {
+                hostClientUI.SetActive(false);
+                clientEnterCodeUI.SetActive(true);
+            }
+            backButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            backButton.gameObject.SetActive(false);
+            hostClientUI.SetActive(true);
+        }
+
+    }
+
+    private void JoinMap()
+    {
+        CreateRelay();        
+    }
+    private void JoinGame()
+    {
+        string joinCode = inputFieldText.text;
+        JoinRelay(joinCode);
+        
+    }
+
     void OnClientConnected(ulong clientId)
     {
         if (clientId == NetworkManager.Singleton.LocalClientId)
         {
             //SetPlayerController();
         }
-    }
-    public void SetPlayerController()
-    {
-
-
-        //pc = NetworkManager.LocalClient.PlayerObject.GetComponent<PlayerController>();
-        //HeroSelectUI.Instance.playerController = pc;
     }
 }

af937ba46a823ff9bf7153fa3f88dbf93f1b5325
Michael Dibonaventura <92171589+granfloyd@users.noreply.github.com>
2024-05-04 21:27:14 -0400

    moving to pc
    changes to mod dmg

--- Assets/Scripts/Modifiers.cs
+++ Assets/Scripts/Modifiers.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class Modifiers : MonoBehaviour
 {
-    public const float MERLIN_BONK_MULTIPLIER = 2f;
-
-    public bool isBonk = false;
+    
+    public bool isStatsChanged = false;
     public int ApplyModifier(int damage)
     {
         int modifiedDamage = damage;
 
-        if (isBonk)
-            modifiedDamage = (int)(modifiedDamage * MERLIN_BONK_MULTIPLIER);
+            
+        modifiedDamage = (int)(modifiedDamage * ModifierSignifiers.MERLIN_EDMG_MULTIPLIER);
 
         return modifiedDamage;
     }
 
+    private void SetModifier(ModifierSignifiers.ModifierIndex id)
+    {
+        switch(id)
+        {
+            case ModifierSignifiers.ModifierIndex.MERLIN_EDMG_MULTIPLIER:
+
+                break;
+
+
+        }
+
+    }
+
+}
+public static class ModifierSignifiers
+{
+    public const float MERLIN_EDMG_MULTIPLIER = 2f;
+    public enum ModifierIndex
+    {
+        MERLIN_EDMG_MULTIPLIER,//100% dmg bonus / 2x dmg 
+        SOMETHINGELSE_DEBUFF//idk maybe like 50% less dmg 
+    }
 }
